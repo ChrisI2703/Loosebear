fd26c916bc88002eb0a197bce9b99e457ad54f04
ChrisI2703 <Ibbotsoc64@my.sunyulster.edu>
2024-05-06 02:08:30 -0400

    Final Project
    Final Project For Computer Game Design

---
+++ Assets/Scripts/Currency.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+   private int Coin = 0;
+   
+   
+   private void OnTriggerEnter(Collider other)
+   {
+	   
+	   if(other.transform.tag == "Coin")
+	   {
+		   Coin++;
+		   Debug.Log(Coin);
+		   Destroy(other.gameObject);
+	   }
+   }
+}
